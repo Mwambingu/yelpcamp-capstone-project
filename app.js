b18f8fa1b642819538e07d9606ac36cecd6d487b
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-11-06 20:01:02 +0300

    Added comments to the code

--- app.js
+++ app.js
@@ -4,21 +4,28 @@ const path = require("path");
 
 main().catch((err) => console.log(err));
 
+// Connecting mongoose to the DB
 async function main() {
     mongoose.connect("mongodb://127.0.0.1:27017/yelpcamp");
     console.log("Connected to DB...");
 }
 
+// Initializing the express app
 app = express();
 port = 3000;
 
+// Setting the native template rendering engine to ejs
 app.set("view engine", "ejs");
+
+// Setting the default views folder to an absolute path
 app.set("views", path.join(__dirname, "views"));
 
+// Routes to the home page
 app.get("/", (req, res) => {
     res.render("home");
 });
 
+// Binds and listens to connections on the specified port
 app.listen(port, () => {
     console.log(`Serving on port: ${port}`);
 });
